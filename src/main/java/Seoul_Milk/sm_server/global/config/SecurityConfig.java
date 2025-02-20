86cb6a32d88d0219f1a626e6dd7266ac1a967874
김영록 <praoo800@konkuk.ac.kr>
2025-02-20 21:40:24 +0900

    feat : securityconfig에 구현한 필터(로그인, jwt)추가 #5

--- src/main/java/Seoul_Milk/sm_server/global/config/SecurityConfig.java
+++ src/main/java/Seoul_Milk/sm_server/global/config/SecurityConfig.java
@@ -1,13 +1,23 @@
 package Seoul_Milk.sm_server.global.config;
 
 import Seoul_Milk.sm_server.global.exception.CustomAuthenticationEntryPoint;
+import Seoul_Milk.sm_server.global.jwt.JWTFilter;
+import Seoul_Milk.sm_server.global.jwt.JWTUtil;
+import Seoul_Milk.sm_server.global.jwt.LoginFilter;
+import Seoul_Milk.sm_server.login.repository.MemberRepository;
+import Seoul_Milk.sm_server.login.repository.RefreshRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,13 +25,31 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
+    private final MemberRepository memberRepository;
 
     // 인증이 필요하지 않은 URL 목록
     private final String[] allowedUrls = {
             "/",
             "/swagger-ui/**",
             "/v3/api-docs/**",
+            "/join",
+            "/login",
     };
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
 
     @Bean
@@ -35,9 +63,21 @@ public class SecurityConfig {
                 .requestMatchers(allowedUrls).permitAll()
                 .anyRequest().authenticated());
 
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+
+        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository, memberRepository), UsernamePasswordAuthenticationFilter.class);
+
         // 예외 처리 설정
         http.exceptionHandling(e -> e
                 .authenticationEntryPoint(customAuthenticationEntryPoint));
+
+        // 세션 처리(stateless로 관리)
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
