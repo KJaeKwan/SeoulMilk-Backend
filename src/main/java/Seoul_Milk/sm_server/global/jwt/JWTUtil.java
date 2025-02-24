aac09cfd8490fea174d8039642d601ea37c8163d
김영록 <praoo800@konkuk.ac.kr>
2025-02-25 04:07:39 +0900

    feat : jwt유효성 검사 시 예외처리 로직 추가(미리 정의한 500에러 관리 advice때문에 jwt에 대한 예외사항이 너무 자세히 노출 -> 보안상의 문제 때문에 미리 정의해둔 예외 이외의 예외가 발생시 모두 INVALID_TOKEN에러로 처리)  #9

--- src/main/java/Seoul_Milk/sm_server/global/jwt/JWTUtil.java
+++ src/main/java/Seoul_Milk/sm_server/global/jwt/JWTUtil.java
@@ -1,7 +1,10 @@
 package Seoul_Milk.sm_server.global.jwt;
 
+import Seoul_Milk.sm_server.global.exception.CustomException;
+import Seoul_Milk.sm_server.global.exception.ErrorCode;
 import Seoul_Milk.sm_server.login.constant.Role;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.SignatureException;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.SecretKey;
@@ -17,23 +20,36 @@ public class JWTUtil {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
-    public String getEmployeeId(String token) {
-
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("employeeId", String.class);
+    public String getEmployeeId(String token){
+        try{
+            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("employeeId", String.class);
+        }catch (Exception e){
+            throw new CustomException(ErrorCode.TOKEN_INVALID);
+        }
     }
 
     public Role getRole(String token) {
-        String roleStr = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
-        return Role.valueOf(roleStr);
+        try{
+            return Role.valueOf(Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class));
+        }catch (Exception e){
+            throw new CustomException(ErrorCode.TOKEN_INVALID);
+        }
     }
 
     public String getCategory(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+        try{
+            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+        }catch (Exception e){
+            throw new CustomException(ErrorCode.TOKEN_INVALID);
+        }
     }
 
     public Boolean isExpired(String token) {
-
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+        try {
+            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+        }catch (SignatureException e){
+            throw new CustomException(ErrorCode.TOKEN_INVALID);
+        }
     }
 
     public String createJwt(String category, String employeeId, String role, Long expiredMs) {
