8f91797fedef2de5779ad12fab7801714b98d49a
shash0423 <shash042319@naver.com>
2025-02-24 12:17:03 +0900

    chore: Swagger description 추가 #8

--- src/main/java/Seoul_Milk/sm_server/login/controller/ReissueController.java
+++ src/main/java/Seoul_Milk/sm_server/login/controller/ReissueController.java
@@ -2,6 +2,8 @@ package Seoul_Milk.sm_server.login.controller;
 
 import Seoul_Milk.sm_server.global.dto.response.SuccessResponse;
 import Seoul_Milk.sm_server.login.service.ReissueService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "토큰 재발급")
 public class ReissueController {
     private final ReissueService reissueService;
     @PostMapping("/reissue")
+    @Operation(summary = "토큰 재발급 API")
     public SuccessResponse<?> reissue(HttpServletRequest request, HttpServletResponse response) {
         return SuccessResponse.ok(reissueService.reissue(request, response));
     }
