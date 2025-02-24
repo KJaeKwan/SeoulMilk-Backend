aab044007a5e278806d245d6cbd5d6ed829ce7f5
김영록 <praoo800@konkuk.ac.kr>
2025-02-24 15:12:52 +0900

    refactor : lazy 로딩 적용 #11

--- src/main/java/Seoul_Milk/sm_server/login/entity/RefreshEntity.java
+++ src/main/java/Seoul_Milk/sm_server/login/entity/RefreshEntity.java
@@ -3,6 +3,7 @@ package Seoul_Milk.sm_server.login.entity;
 import Seoul_Milk.sm_server.global.refresh.RefreshToken;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,7 +26,7 @@ public class RefreshEntity {
     @Column(name = "REPOSITORY_ID")
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private MemberEntity memberEntity;
 
