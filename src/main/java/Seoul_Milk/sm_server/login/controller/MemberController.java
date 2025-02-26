2ff2878213c596d368172edb3821cb63aa66f8dd
shash0423 <shash042319@naver.com>
2025-02-26 14:12:51 +0900

    feat: 비밀번호 변경 API 구현 #18

--- src/main/java/Seoul_Milk/sm_server/login/controller/MemberController.java
+++ src/main/java/Seoul_Milk/sm_server/login/controller/MemberController.java
@@ -2,14 +2,14 @@ package Seoul_Milk.sm_server.login.controller;
 
 import Seoul_Milk.sm_server.global.annotation.CurrentMember;
 import Seoul_Milk.sm_server.global.dto.response.SuccessResponse;
+import Seoul_Milk.sm_server.login.dto.UpdatePwDTO;
 import Seoul_Milk.sm_server.login.entity.MemberEntity;
 import Seoul_Milk.sm_server.login.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/members")
@@ -22,8 +22,16 @@ public class MemberController {
     @GetMapping("/myPage")
     @Operation(summary = "마이페이지")
     public SuccessResponse<MemberEntity> myPage(@CurrentMember MemberEntity member) {
-        System.out.println("member.getEmployeeId() = " + member.getEmployeeId());
         MemberEntity my = memberService.getMember(member.getEmployeeId());
         return SuccessResponse.ok(my);
+    }
+
+    @PatchMapping("/password")
+    @Operation(summary = "비밀번호 변경")
+    public SuccessResponse<String> updatePassword(
+            @CurrentMember MemberEntity member,
+            @Valid @RequestBody UpdatePwDTO request) {
+        memberService.updatePw(member.getId(), request);
+        return SuccessResponse.ok("비밀번호 변경에 성공했습니다.");
     }
 }
