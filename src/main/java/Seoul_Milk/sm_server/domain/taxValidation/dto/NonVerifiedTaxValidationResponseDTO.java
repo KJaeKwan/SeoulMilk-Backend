e63f777ffe6390da4cde33758f16b1754a689d2e
김영록 <praoo800@konkuk.ac.kr>
2025-03-02 12:44:23 +0900

    feat : 간편이증 전 진위여부 호출 api에서 key값이 추가 응답으로 오도록 수정 #26

--- src/main/java/Seoul_Milk/sm_server/domain/taxValidation/dto/NonVerifiedTaxValidationResponseDTO.java
+++ src/main/java/Seoul_Milk/sm_server/domain/taxValidation/dto/NonVerifiedTaxValidationResponseDTO.java
@@ -1,15 +1,17 @@
 package Seoul_Milk.sm_server.domain.taxValidation.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-@Schema(description = "간편인증 안한 상태에서의 진위여부 요청 api response body")
+@Schema(description = "간편인증 안한 상태에서의 진위여부 요청 api response")
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class NonVerifiedTaxValidationResponseDTO {
-    @Schema(description = "잡 인덱스")
-    private int jobIndex;
-    @Schema(description = "스레드 인덱스")
-    private int threadIndex;
-    @Schema(description = "트렌젝션 아이디")
-    private String jti;
-    @Schema(description = "추가 인증 시간")
-    private Long twoWayTimestamp;
+    @Schema(description = "간편인증 후 api에서 사용할 key값")
+    private String key;
 }
