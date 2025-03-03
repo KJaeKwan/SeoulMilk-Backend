37570eaaa228b34cb1311529f5217ebcf268085f
shash0423 <shash042319@naver.com>
2025-03-03 11:11:54 +0900

    chore: 불필요한 import 제거 #38

--- src/main/java/Seoul_Milk/sm_server/login/controller/MemberController.java
+++ src/main/java/Seoul_Milk/sm_server/login/controller/MemberController.java
@@ -4,15 +4,12 @@ import Seoul_Milk.sm_server.global.annotation.CurrentMember;
 import Seoul_Milk.sm_server.global.dto.response.SuccessResponse;
 import Seoul_Milk.sm_server.login.dto.VerifyPwDTO;
 import Seoul_Milk.sm_server.login.dto.request.UpdatePwDTO;
-import Seoul_Milk.sm_server.login.dto.request.UpdateRoleDTO;
-import Seoul_Milk.sm_server.login.dto.response.MemberResponse;
 import Seoul_Milk.sm_server.login.entity.MemberEntity;
 import Seoul_Milk.sm_server.login.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
