bf710cb028e70bfe69840a84af1c4c03f8687c5d
김영록 <praoo800@konkuk.ac.kr>
2025-03-06 21:58:49 +0900

    feat : 임시저장된 이미지 전체 조회 api 응답값에 파일id 추가 #72

--- src/main/java/Seoul_Milk/sm_server/domain/image/dto/ImageResponseDTO.java
+++ src/main/java/Seoul_Milk/sm_server/domain/image/dto/ImageResponseDTO.java
@@ -10,11 +10,13 @@ public class ImageResponseDTO {
 
     @Schema(description = "임시저장 이미지 단건 조회 응답 DTO")
     public record GetOne(
+            @Schema(description = "이미지 ID") Long imageId,
             @Schema(description = "이미지 URL") String imageUrl,
             @Schema(description = "날짜") LocalDate uploadDate
     ) {
         public static GetOne from(Image image) {
             return new GetOne(
+                    image.getId(),
                     image.getImageUrl(),
                     image.getUploadDate()
             );
