459335a2fd57799b9aded4dd34f32d041fd791c4
김영록 <praoo800@konkuk.ac.kr>
2025-04-03 15:48:11 +0900

    test : 진위여부 검사(간편인증 후 진위여부 저장 로직) 테스트 코드 작성 #160

--- src/main/java/Seoul_Milk/sm_server/domain/taxInvoice/dto/validation/request/VerifiedTaxValidationRequestDTO.java
+++ src/main/java/Seoul_Milk/sm_server/domain/taxInvoice/dto/validation/request/VerifiedTaxValidationRequestDTO.java
@@ -1,10 +1,12 @@
 package Seoul_Milk.sm_server.domain.taxInvoice.dto.validation.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 
 @Schema(description = "간편인증한 상태에서의 진위여부 요청 api request")
 @Getter
+@Builder
 public class VerifiedTaxValidationRequestDTO {
     private String key;
 }
--- src/test/java/Seoul_Milk/sm_server/domain/taxInvoice/service/TaxInvoiceValidationServiceImplTest.java
+++ src/test/java/Seoul_Milk/sm_server/domain/taxInvoice/service/TaxInvoiceValidationServiceImplTest.java
@@ -1,12 +1,36 @@
 package Seoul_Milk.sm_server.domain.taxInvoice.service;
 
 
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.APPROVAL_NO;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.CONTRACTOR_REG_NUMBER;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.ID;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.IDENTITY;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.LOGIN_TYPE;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.LOGIN_TYPE_LEVEL;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.ORGANIZATION;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.ORIGINAL_APPROVAL_NO;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.PHONE_NO;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.REPORTING_DATE;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.SUPPLIER_REG_NUMBER;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.SUPPLY_VALUE;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.TELECOM;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.CodefParameters.USER_NAME;
 import static Seoul_Milk.sm_server.domain.taxInvoice.enums.ProcessStatus.APPROVED;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.ProcessStatus.PENDING;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.ProcessStatus.REJECTED;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.TwoWayInfo.JOB_INDEX;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.TwoWayInfo.JTI;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.TwoWayInfo.THREAD_INDEX;
+import static Seoul_Milk.sm_server.domain.taxInvoice.enums.TwoWayInfo.TWO_WAY_TIMESTAMP;
+import static Seoul_Milk.sm_server.global.common.exception.ErrorCode.CODEF_INTERANL_SERVER_ERROR;
+import static Seoul_Milk.sm_server.global.common.exception.ErrorCode.CODEF_NEED_AUTHENTICATION;
 import static Seoul_Milk.sm_server.util.TaxDataCreatorUtil.createTaxInvoice;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
@@ -16,6 +40,7 @@ import Seoul_Milk.sm_server.domain.taxInvoice.dto.validation.request.TaxInvoiceI
 import Seoul_Milk.sm_server.domain.taxInvoice.dto.validation.response.NonVerifiedTaxValidationResponseDTO;
 import Seoul_Milk.sm_server.domain.taxInvoice.entity.TaxInvoice;
 import Seoul_Milk.sm_server.domain.taxInvoice.repository.TaxInvoiceRepository;
+import Seoul_Milk.sm_server.global.common.exception.CustomException;
 import Seoul_Milk.sm_server.global.infrastructure.redis.RedisUtils;
 import Seoul_Milk.sm_server.mock.container.TestContainer;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -51,6 +76,7 @@ public class TaxInvoiceValidationServiceImplTest {
         mockCreateRequestThread();
         mockMemberUUID();
         mockCodefRequestProduct();
+        mockRedisgetCodefApiResponse();
         threadMocking();
     }
     private void mockMemberSetting(){
@@ -107,6 +133,124 @@ public class TaxInvoiceValidationServiceImplTest {
         doNothing().when(thread).start();
     }
 
+    /**
+     * 진위여부 true인 경우
+     * @throws JsonProcessingException
+     * @throws UnsupportedEncodingException
+     * @throws InterruptedException
+     */
+    private void mockCodefResultRealResponse()
+            throws JsonProcessingException, UnsupportedEncodingException, InterruptedException {
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", "CF-00000");
+        result.put("message", "정상 처리되었습니다.");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("resAuthenticity", "1"); // 진위여부 true
+
+        Map<String, Object> mockResponseMap = new HashMap<>();
+        mockResponseMap.put("result", result);
+        mockResponseMap.put("data", data);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String mockResponse = objectMapper.writeValueAsString(mockResponseMap);
+
+        when(easyCodef.requestCertification(
+                anyString(),
+                eq(EasyCodefServiceType.DEMO),
+                any(HashMap.class)
+        )).thenReturn(mockResponse);
+    }
+
+    /**
+     * 진위여부 false인 경우
+     * @throws JsonProcessingException
+     * @throws UnsupportedEncodingException
+     * @throws InterruptedException
+     */
+    private void mockCodefResultFakeResponse()
+            throws JsonProcessingException, UnsupportedEncodingException, InterruptedException {
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", "CF-00000");
+        result.put("message", "정상 처리되었습니다.");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("resAuthenticity", "0"); // 진위여부 false
+
+        Map<String, Object> mockResponseMap = new HashMap<>();
+        mockResponseMap.put("result", result);
+        mockResponseMap.put("data", data);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String mockResponse = objectMapper.writeValueAsString(mockResponseMap);
+
+        when(easyCodef.requestCertification(
+                anyString(),
+                eq(EasyCodefServiceType.DEMO),
+                any(HashMap.class)
+        )).thenReturn(mockResponse);
+    }
+
+    /**
+     * 간편인증 안 하고 요청 시
+     * @throws UnsupportedEncodingException
+     * @throws JsonProcessingException
+     * @throws InterruptedException
+     */
+    private void mockCodefResultNotAuthResponse()
+            throws UnsupportedEncodingException, JsonProcessingException, InterruptedException {
+        Map<String, Object> result = new HashMap<>();
+        result.put("code", "CF-03002");
+        result.put("message", "인증 필요");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("resAuthenticity", "0"); // 진위여부 false
+
+        Map<String, Object> mockResponseMap = new HashMap<>();
+        mockResponseMap.put("result", result);
+        mockResponseMap.put("data", data);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String mockResponse = objectMapper.writeValueAsString(mockResponseMap);
+
+        when(easyCodef.requestCertification(
+                anyString(),
+                eq(EasyCodefServiceType.DEMO),
+                any(HashMap.class)
+        )).thenReturn(mockResponse);
+    }
+
+    private void mockRedisgetCodefApiResponse(){
+        Map<String, Object> authResponse = new HashMap<>();
+        authResponse.put(ORGANIZATION.getKey(), "0004"); // 기관 코드
+        authResponse.put(ID.getKey(), MEMBER_UNIQUE_ID); // 사용자 ID
+        authResponse.put(LOGIN_TYPE.getKey(), "5");      // 로그인 타입
+        authResponse.put(JOB_INDEX.name(), "0");
+        authResponse.put(THREAD_INDEX.name(), "0");
+        authResponse.put(JTI.name(), "5858a9371e9c43e78f0f2739a941d529");
+        authResponse.put(TWO_WAY_TIMESTAMP.name(), "1569910218149");
+
+        Map<String, Object> commonResponse = new HashMap<>();
+        commonResponse.put(LOGIN_TYPE_LEVEL.getKey(), "1");
+        commonResponse.put(USER_NAME.getKey(), "김영록");
+        commonResponse.put(PHONE_NO.getKey(), "01012345678");
+        commonResponse.put(IDENTITY.getKey(), "010327");
+        commonResponse.put(TELECOM.getKey(), "1");
+
+        Map<String, Object> firstResponse = new HashMap<>();
+        firstResponse.put(SUPPLIER_REG_NUMBER.getKey(), "1");
+        firstResponse.put(CONTRACTOR_REG_NUMBER.getKey(), "1");
+        firstResponse.put(APPROVAL_NO.getKey(), "1");
+        firstResponse.put(REPORTING_DATE.getKey(), "2024-03-26");
+        firstResponse.put(SUPPLY_VALUE.getKey(), "1");
+        firstResponse.put(ORIGINAL_APPROVAL_NO.getKey(), "1");
+
+        when(testContainer.redisUtils.getCodefApiResponse(MEMBER_UNIQUE_ID)).thenReturn(authResponse);
+        when(testContainer.redisUtils.getCodefApiResponse(MEMBER_UNIQUE_ID + "common")).thenReturn(commonResponse);
+        when(testContainer.redisUtils.getCodefApiResponse(MEMBER_UNIQUE_ID + "first")).thenReturn(firstResponse);
+    }
+
+
     @Test
     @DisplayName("초기 인증 신청 시 테스트")
     void initialCodefRequest() throws InterruptedException {
@@ -136,5 +280,34 @@ public class TaxInvoiceValidationServiceImplTest {
         assertThat(response.getKey()).isEqualTo(MEMBER_UNIQUE_ID);
     }
 
+    @Test
+    @DisplayName("진위여부 파악 테스트(세금계산서가 진짜 일떄)")
+    void realTaxInvoice() throws UnsupportedEncodingException, JsonProcessingException, InterruptedException {
+        mockCodefResultRealResponse();
+        TaxInvoice taxInvoice = createTaxInvoice(1L, "1", PENDING, "1", "1", "2024-03-26", null, "서울우유 1", testMember);
+        testContainer.taxInvoiceRepository.save(taxInvoice);
+        testContainer.taxInvoiceValidationService.verifiedTaxValidation(MEMBER_UNIQUE_ID);
+        assertThat(taxInvoice.getProcessStatus()).isEqualTo(APPROVED);
+    }
 
+    @Test
+    @DisplayName("진위여부 파악 테스트(세금계산서가 가짜 일때)")
+    void fakeTaxInvoice() throws UnsupportedEncodingException, JsonProcessingException, InterruptedException {
+        mockCodefResultFakeResponse();
+        TaxInvoice taxInvoice = createTaxInvoice(1L, "1", PENDING, "1", "1", "2024-03-26", null, "서울우유 1", testMember);
+        testContainer.taxInvoiceRepository.save(taxInvoice);
+        testContainer.taxInvoiceValidationService.verifiedTaxValidation(MEMBER_UNIQUE_ID);
+        assertThat(taxInvoice.getProcessStatus()).isEqualTo(REJECTED);
+    }
+
+    @Test
+    @DisplayName("진위여부 파악 테스트(간편인증을 하지 않았을 때)")
+    void notAuth() throws UnsupportedEncodingException, JsonProcessingException, InterruptedException {
+        mockCodefResultNotAuthResponse();
+        TaxInvoice taxInvoice = createTaxInvoice(1L, "1", PENDING, "1", "1", "2024-03-26", null, "서울우유 1", testMember);
+        testContainer.taxInvoiceRepository.save(taxInvoice);
+        assertThatThrownBy(() -> testContainer.taxInvoiceValidationService.verifiedTaxValidation(MEMBER_UNIQUE_ID))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(CODEF_NEED_AUTHENTICATION.getMessage());
+    }
 }
